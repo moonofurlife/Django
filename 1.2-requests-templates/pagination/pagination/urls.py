3abb8fe677f3f51cacbca1d0850eb235534241c5
Ann <anna.rogazinskaya@gmail.com>
2023-10-02 19:28:14 +0300

    initial commit

--- 1.2-requests-templates/pagination/pagination/urls.py
+++ 1.2-requests-templates/pagination/pagination/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from stations.views import index, bus_stations
 
 urlpatterns = [
-    path('', include('stations.urls')),
+    path('', index, name='index'),
+    path('bus_stations/', bus_stations, name='bus_stations')
 ]
