3721bb8701ffd7ef0036049aa6ffe072921b6d03
Ann <anna.rogazinskaya@gmail.com>
2023-10-21 23:21:23 +0300

    initial commit

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name + self.description
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, related_name='measurements', on_delete=models.CASCADE)
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    changed_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='measurement_images/', null=True, blank=True)
+
--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import SensorsView, SensorView, SensorDeleteView, MeasurementsAddView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorsView.as_view()),
+    path('sensors/<id>/', SensorView.as_view()),
+    path('measurements/', MeasurementsAddView.as_view()),
+    path('sensor_delete/<id>/', SensorDeleteView.as_view()),
 ]
